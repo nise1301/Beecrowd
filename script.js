ffc6c283578e1db8996dda017c7bbb406bb69d1b
Denise Castella <disoares@edu.unifor.br>
2024-05-28 12:40:13 -0300

    1182 - Coluna da Matriz
    Resolução comentada do problema 1182 do Beecrowd em Javascript

--- script.js
+++ script.js
@@ -39,3 +39,47 @@ if(operacao=='S'){
 if(operacao=='M'){
     console.log((soma/12).toFixed(1))
 }*/
+
+/*
+//problema 1182 do Beecrowd
+
+//inicializa matriz vazia
+var matriz = []
+
+//inicializa variavel soma
+var soma = 0.0
+
+//inicializa o indice que irá iterar sobre a coluna
+var indice = parseInt(lines.shift())
+
+//inicializa a operação a ser escolhida pelo usuario
+var operacao = lines.shift()
+
+//cria um laço for para criar os espaços dentro da matriz
+//o primeiro laço cria os arrays dentro da matriz conforme cada indice i
+for(let i= 0; i<12; i++){
+    matriz[i] = []
+    //o segundo for coloca números digitados pelo juiz online do Beecrowd em cada posição, conforme as iterações de linha i e coluna j
+    for(let j = 0;j<12;j++){
+        matriz [i][j] = parseFloat(lines.shift())
+    }
+}
+
+//esse laço percorre a matriz criada acima, usando as iterações da linha i e da coluna j
+for(let i = 0; i<12;i++){
+    for( let j = 0; j<12; j++){
+        //aqui temos uma decisão: se o indice da coluna j for igual ao indice digitado pelo juiz virtual
+        //colocamos o valor de i e j na variavel soma e juntamos com os demais elementos já contidos lá
+        if(j==indice){
+            soma += matriz [i][j]
+        }
+    }
+}
+//se a operação digitada pelo juiz vitual for S de soma, deverá ser impresso o valor da soma no console
+if(operacao=='S'){
+    console.log(soma.toFixed(1))
+}
+//se a operação digitada pelo juiz vitual for M de média, deverá ser impresso o valor da média no console
+if(operacao=='M'){
+    console.log((soma/12).toFixed(1))
+}*/
