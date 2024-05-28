ca8b9169fb0c97690aa88aae7e204d4082079070
Denise Castella <disoares@edu.unifor.br>
2024-05-28 12:07:16 -0300

    1181 comentado javascript
    Código comentado para resolução do problema 1181 do Beecrowd em javascript

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 //Beecrowd 1181 em javascript
+/*
+// Inicializa uma matriz vazia
 var matriz = []
+
+// Inicializa a variável soma com 0.0
 var soma = 0.0
+
+// Lê o índice da linha que será somada ou terá a média calculada
 var indice = parseInt(lines.shift())
+
+// Lê a operação a ser realizada: 'S' para soma e 'M' para média
 operacao = lines.shift()
 
+// Preenche a matriz com valores reais
 for(let i = 0; i<12;i++){
     matriz[i] = []
     for(let j = 0; j<12;j++){
@@ -12,6 +21,7 @@ for(let i = 0; i<12;i++){
     }
 }
 
+// Soma os elementos da linha especificada pelo índice
 for(let i = 0; i<12;i++){
     for(let j = 0; j<12; j++){
         if(i==indice){
@@ -20,10 +30,12 @@ for(let i = 0; i<12;i++){
     }
 }
 
+// Se a operação for 'S', imprime a soma dos elementos da linha
 if(operacao=='S'){
     console.log(soma.toFixed(1))
 }
 
+// Se a operação for 'M', imprime a média dos elementos da linha
 if(operacao=='M'){
     console.log((soma/12).toFixed(1))
-}
+}*/
